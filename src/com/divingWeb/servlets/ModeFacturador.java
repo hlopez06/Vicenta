30577d7b314f8dd892eda9be7684b8e2736c3d6a
hlopez06 <hernan.h.lopez@gmail.com>
2013-02-21 16:51:36 -0300

    Creando Factura y produtos 1er parte

--- src/com/divingWeb/servlets/ModeFacturador.java
+++ src/com/divingWeb/servlets/ModeFacturador.java
@@ -9,12 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ModeVenta extends HttpServlet {
-	
+import com.divingWeb.facturador.Factura;
+
+public class ModeFacturador extends HttpServlet {
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
 				RequestDispatcher disp;
+				
+//				cargar clientes
+				
+				setAttribute("factura", new Factura());
 				
 				disp = getServletContext().getRequestDispatcher("/JSP/templates/venta.jsp");
 				
